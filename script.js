33682aa52439a8da647586691ad071abbc4a7177
Chokseys2022 <choksey2022@gmail.com>
2024-02-02 14:08:17 -0800

    done

--- script.js
+++ script.js
@@ -91,3 +91,105 @@ class Adventurer extends Character {
     this.health += 10;
   }
 }
+// Part 4: Class Uniforms
+// Using the static keyword:
+// Add a static MAX_HEALTH property to the Character class, equal to 100.
+class Character {
+  static MAX_HEALTH = 100;
+
+  constructor(name) {
+    this.name = name;
+    this.health = Character.MAX_HEALTH;
+    this.inventory = [];
+  }
+}
+
+class Adventurer extends Character {
+  static ROLES = ["Fighter", "Healer", "Wizard"];
+}
+
+// Part 5: Gather your Party
+// Factories are classes that generate objects according to the factory’s instance properties.
+// As an example, let’s look at how we might create many “healer” role adventurers using a factory:
+class AdventurerFactory {
+  constructor(role) {
+    this.role = role;
+    this.adventurers = [];
+  }
+  generate(name) {
+    const newAdventurer = new Adventurer(name, this.role);
+    this.adventurers.push(newAdventurer);
+  }
+  findByIndex(index) {
+    return this.adventurers[index];
+  }
+  findByName(name) {
+    return this.adventurers.find((a) => a.name === name);
+  }
+}
+
+const healers = new AdventurerFactory("Healer");
+const robin = healers.generate("Robin");
+
+//   Part 6: Developing Skills
+// Create an additional method, duel(), for the Adventurer class with the following functionality:
+// Part 6: Developing Skills
+class Adventurer extends Character {
+  constructor(name, role) {
+    super(name);
+    this.role = role;
+    this.inventory.push("bedroll", "50 gold coins");
+    this.level = 1;
+    this.experience = 0;
+    this.skills = [];
+  }
+
+  scout() {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
+  }
+
+  levelUp() {
+    console.log(`${this.name} has leveled up!`);
+    this.level++;
+    this.health += 10;
+  }
+
+  duel(opponent) {
+    console.log(`Duel between ${this.name} and ${opponent.name} begins!`);
+
+    while (this.health > 50 && opponent.health > 50) {
+      const playerRoll = this.roll();
+      const opponentRoll = opponent.roll();
+
+      console.log(
+        `${this.name} rolled a ${playerRoll}, ${opponent.name} rolled a ${opponentRoll}`
+      );
+
+      if (playerRoll > opponentRoll) {
+        opponent.health -= 1;
+        console.log(
+          `${this.name} wins this round! ${opponent.name} loses 1 health.`
+        );
+      } else if (opponentRoll > playerRoll) {
+        this.health -= 1;
+        console.log(
+          `${opponent.name} wins this round! ${this.name} loses 1 health.`
+        );
+      } else {
+        console.log("It's a tie! No damage dealt.");
+      }
+
+      console.log(
+        `${this.name}'s health: ${this.health}, ${opponent.name}'s health: ${opponent.health}`
+      );
+    }
+
+    const winner = this.health > 50 ? this : opponent;
+    console.log(
+      `${winner.name} wins the duel with ${winner.health} health remaining!`
+    );
+  }
+}
+
+
