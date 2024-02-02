d4b4c0b062e93295627ae068d264e3143c340c20
Chokseys2022 <choksey2022@gmail.com>
2024-02-02 13:23:23 -0800

    part 2 done

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-// Let’s model a simple adventurer with basic properties such as health and an inventory. 
+// Let’s model a simple adventurer with basic properties such as health and an inventory.
 // We will call the adventurer “Robin.”
 // const adventurer = {
 //     name: "Robin",
@@ -7,7 +6,7 @@
 //     inventory: ["sword", "potion", "artifact"]
 //   };
 // adventurer.inventory[0];
-// console.log(adventurer);  
+// console.log(adventurer);
 
 // Let’s give Robin a companion to travel with – a furry friend they call “Leo.”
 // const adventurer = {
@@ -19,7 +18,7 @@
 //       type: "Cat"
 //     }
 //   };
-  
+
 // Next, give Robin’s feline friend a friend of his own:
 // Add a “companion” sub-object to “Leo” with the following properties:
 // The companion’s name is “Frank.”
@@ -27,25 +26,43 @@
 // The companion has its own belongings, which includes a small hat and sunglasses.
 
 const adventurer = {
-    name: "Robin",
-    health: 10,
-    inventory: ["sword", "potion", "artifact"],
-    companion: {
-      name: "Leo",
-      type: "Cat",
-      belongings: {
-        name: "Frank",
-        type: "Flea",
-        accessories: ["small hat", "sunglasses"]
-      }
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  companion: {
+    name: "Leo",
+    type: "Cat",
+    belongings: {
+      name: "Frank",
+      type: "Flea",
+      accessories: ["small hat", "sunglasses"],
     },
-    roll: function () {
-      // Generate a random number between 1 and 6 for a typical six-sided die
-      return Math.floor(Math.random() * 6) + 1;
+  },
+
+  //   Now we have a method for “dice rolls,” a common way to handle outcomes in adventuring games. Test this method by calling adventurer.roll() a few times.
+  roll: function () {
+    return Math.floor(Math.random() * 6) + 1;
+  },
+};
+// Test the roll method
+const diceRollResult = adventurer.roll();
+console.log(`${adventurer.name} rolled a ${diceRollResult}!`);
+
+
+// Part 2: Class Fantasy
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
     }
-  };
-  
-  // Test the roll method
-  const diceRollResult = adventurer.roll();
-  console.log(`${adventurer.name} rolled a ${diceRollResult}!`);
-  
+  }
+
+//   Re-create Robin
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
